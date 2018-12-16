69e3ed989ed6acb0c5bc6433e87a663314f8c649
Niv Suresh <nivsuresh06@gmail.com>
2018-12-16 21:58:28 +0000

    Setting state with axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CryptoJS from 'crypto-js'
+import Axios from 'axios';
+
+const publicKey = '306e2c7a911a16b8c95ac6996801dd65'
+const privateKey = 'c92da84ea1eaa998b871e88d942448bc4cefd9dc'
+const ts = new Date().getTime()
+const hash = CryptoJS.MD5(ts + privateKey + publicKey).toString()
+let offset = 0
+
+// Axios.get(`http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=100&offset=${offset}`)
+// .then((res) => {
+//   console.log(res.data.data.results)
+//   console.log('first')
+//   offset = offset + 100
+//   Axios.get(`http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=100&offset=${offset}`)
+//   .then((result) => {
+//     console.log(result.data.data.results)
+//   })
+// })
+// .catch((err) => {
+//   console.log(err)
+// })
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+       chars: []
+    }
+ }
+
+ componentDidMount() {
+   console.log('mounted')
+   Axios.get(`http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=100&offset=${offset}`)
+   .then((char) => {
+     console.log(char.data.data.results[0])
+     this.setState({
+       chars: [...this.state.chars, char.data.data.results]
+     })
+   })
+ }
   render() {
     return (
       <div className="App">
