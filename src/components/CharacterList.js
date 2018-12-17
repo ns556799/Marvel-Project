6870e0fc0138d9bbd6f9e31bd83406b95a4b1655
Niv Suresh <nivsuresh06@gmail.com>
2018-12-17 12:47:13 +0000

    Create CharacterList.js

---
+++ src/components/CharacterList.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import CharacterItem from './CharacterItem'
+
+class CharacterList extends Component {
+    render() {
+        const data = this.props.data
+        return(
+           data.map((item, i) => {
+               return <CharacterItem key = {i} data={item} />
+           }) 
+        )
+    }
+}
+
+
+export default CharacterList
