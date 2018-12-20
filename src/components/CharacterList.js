ec8c7c2b9215dda1809cd29ac95e38becefc38c3
Niv Suresh <nivsuresh06@gmail.com>
2018-12-20 19:05:00 +0000

    Update CharacterList.js

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,14 +2,14 @@ import React, {Component} from 'react'
 import CharacterItem from './CharacterItem'
 
 class CharacterList extends Component {
-    render() {
-        const data = this.props.data
-        return(
-           data.map((item, i) => {
-               return <CharacterItem key = {i} data={item} />
-           })
-        )
-    }
+  render() {
+    const data = this.props.data
+    return (
+      data.map((item, i) => {
+        return <CharacterItem key={i} data={item} />
+      })
+    )
+  }
 }
 
 export default CharacterList
