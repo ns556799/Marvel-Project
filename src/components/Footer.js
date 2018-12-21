67a612afdade263b550edd47ce18d92ae471ce8e
Niv Suresh <nivsuresh06@gmail.com>
2018-12-21 12:35:27 +0000

    Footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../css/Header.css'
+
+const Footer = () => {
+  return (
+    <footer className='app-footer'>
+      <div>
+        <div className='wrap'>
+          <div className='app-footer__text'>
+            Footer
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
