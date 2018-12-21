7e4ab1e1f6f34fe453704b1dbeb9cb9fbd3eda5f
Niv Suresh <nivsuresh06@gmail.com>
2018-12-21 12:34:35 +0000

    Cookiebar + css

---
+++ src/components/Cookiebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../css/Cookiebar.css'
+
+const Cookiebar = () => {
+  return (
+    <div className='app-cookie'>
+      <div>
+        <div className='wrap'>
+          Cookiebar
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Cookiebar
