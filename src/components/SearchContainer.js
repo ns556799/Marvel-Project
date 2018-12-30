ddfebac104db0c4093e09e1d1f70a607c5f6bd0d
Niv Suresh <nivsuresh06@gmail.com>
2018-12-30 14:50:42 +0000

    Update SearchContainer.js

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react'
 import Axios from 'axios'
-import ResItem from './ResItem'
 
 import '../css/SearchContainer.css'
 import CryptoJS from 'crypto-js'
+import ResultList from './ResultList'
 
 let API_PUBLIC_KEY = process.env.REACT_APP_API_PUBLIC_KEY
 let API_PRIVATE_KEY = process.env.REACT_APP_API_PRIVATE_KEY
@@ -11,50 +11,69 @@ let API_PRIVATE_KEY = process.env.REACT_APP_API_PRIVATE_KEY
 const ts = new Date().getTime()
 const hash = CryptoJS.MD5(ts + API_PRIVATE_KEY + API_PUBLIC_KEY).toString()
 
-let timer = null
-
 class SearchContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      inputValue: ''
+      inputValue: '',
+      charResults: [],
+      selectedResults: []
     }
   }
+
   updateInputValue(e) {
     this.setState({
       inputValue: e.target.value
     })
   }
 
-  renderResults(e) {
-    if (this.state.inputValue.length) {
-      clearTimeout(timer)
-      timer = setTimeout(() => {
-        const searchQuery = encodeURIComponent((this.state.inputValue).trim())
-        if (searchQuery.length) {
-          console.log(searchQuery)
-          let searchRes = []
-          Axios.get(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${searchQuery}&ts=${ts}&apikey=${API_PUBLIC_KEY}&hash=${hash}`)
-            .then((res) => {
-              searchRes = [...searchRes, res.data.data.results]
-              console.log(typeof (searchRes))
-              const searchChar = searchRes[0]
-              return Object.keys(searchRes[0]).map((key, index) => <ResItem key={key} data={searchChar[index]} />)
-            })
-            .catch((err) => { console.log(err) })
-        }
-      }, 1000)
-    } else {
+  searchCharacters() {
+    const searchQuery = this.state.inputValue
+    Axios.get(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${searchQuery}&ts=${ts}&apikey=${API_PUBLIC_KEY}&hash=${hash}`)
+      .then((res) => {
+        this.setState({
+          charResults: [...this.state.charResults, [].concat.apply([], res.data.data.results)]
+        })
+      })
+  }
+
+  saveCharacter(e, name) {
+    console.log('add', e.target.dataset.name)
+    this.setState({
+      selectedResults: [...this.state.selectedResults, e.target.dataset.name]
+    })
+  }
+  removeCharacter(e, name) {
+    console.log('remove', e.target.dataset.name)
+    let array = [...this.state.selectedResults] // make a separate copy of the array
+    let index = array.indexOf(e.target.dataset.name)
+    if (index !== -1) {
+      array.splice(index, 1)
+      this.setState({selectedResults: array})
     }
   }
+
+  onCloseResultList() {
+    this.props.closeResults(this.state.selectedResults)
+    this.setState({
+      inputValue: '',
+      charResults: [],
+      selectedResults: []
+    })
+  }
+
   render() {
     return (
       <div className='app-searchbar'>
         <div className='wrap'>
           <div className='app-searchbar__wrapper'>
-            <input value={this.state.inputValue} onChange={e => this.updateInputValue(e)} onKeyDown={e => this.renderResults(e)} />
+            <input value={this.state.inputValue} onChange={e => this.updateInputValue(e)} />
+            <button onClick={this.searchCharacters.bind(this)}>Search</button>
           </div>
-          {this.renderResults(this.state.inputValue)}
+          { this.state.charResults.map((item, i) => {
+            return <ResultList key={i} data={item} save={this.saveCharacter.bind(this)}
+              remove={this.removeCharacter.bind(this)} closeRes={this.onCloseResultList.bind(this)} />
+          })}
         </div>
       </div>
     )
